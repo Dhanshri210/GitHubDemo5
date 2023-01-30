210008f026e8fa2a63de8533c9677953cc780fd2
Dhanshri210 <dhanshriithawale@gmail.com>
2023-01-30 22:45:41 +0530

    Changes By Shree5

---
+++ src/main/java/com/example/demo/GitHUbDemo5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHUbDemo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHUbDemo5Application.class, args);
+	}
+
+}
